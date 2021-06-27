4300260cd58db5011e485a2e26cffeb53c699527
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-06-27 22:55:29 +0700

    working on function

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(
 
-  <ul>
-  <li>cat</li>
-  
-  <li>dog</li>
-  
-  <li>sheep</li>
-</ul>
+function MyInfo() {
+  return (
+    <div className='contain'>
+      <h1>HOÀNG</h1>
+      <p>I like to watch himymt
+        Im 20
+        I study computer engineering in University of Technology
+      </p>
+      <ul>
+        <li>God father</li>
+        <li>Old man 100 years old climb out the window</li>
+        <li>The Hobiit</li>
+      </ul>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <MyInfo/>
 ,document.getElementById('root'));
 
 
